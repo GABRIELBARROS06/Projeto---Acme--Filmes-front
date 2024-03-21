7b387d4c030fa94223e96001778ab9917784b179
GABRIEL DE BARROS GOMES <23111782@CFP127ALUNO.LOCAL>
2024-03-21 17:22:50 -0300

    BLABLA

--- js/main.js
+++ js/main.js
@@ -19,17 +19,6 @@ async function mostrarCards(){
     const container2 = document.getElementById('container2')
     const filmes = await getFilmes()
 
-    // for (let i = 0; i < filmes.length; i++) {
-    //     if(i < 5){
-    //         const card = criarCards(filmes)
-    //         container.appendChild(card)
-    //         console.log(card)        
-    //     }else if(i < 10){
-    //         const card = criarCards(filmes)
-    //         container2.appendChild(card)
-    //         console.log()
-    //     }        
-    // }
 
     filmes.forEach(filme => {
         const card = criarCards(filme)
