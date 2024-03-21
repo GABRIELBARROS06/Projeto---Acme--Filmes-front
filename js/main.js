c1cdb4a8301557ca2b64f4213dcc15a4823be284
GABRIEL DE BARROS GOMES <23111782@CFP127ALUNO.LOCAL>
2024-03-21 12:21:36 -0300

    Terminar na Aula

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import { getFilme, getFilmes } from "./filmes.js"
+
+function criarCards(filme){
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const img = document.createElement('img')
+    img.src = filme.foto_capa
+    img.classList.add('capa')
+
+    card.append(img)
+    return card    
+}
+
+async function mostrarCards(){
+    const container = document.getElementById('container')
+    const container2 = document.getElementById('container2')
+    const filmes = await getFilmes()
+
+    // for (let i = 0; i < filmes.length; i++) {
+    //     if(i < 5){
+    //         const card = criarCards(filmes)
+    //         container.appendChild(card)
+    //         console.log(card)        
+    //     }else if(i < 10){
+    //         const card = criarCards(filmes)
+    //         container2.appendChild(card)
+    //         console.log()
+    //     }        
+    // }
+
+    filmes.forEach(filme => {
+        const card = criarCards(filme)
+        container.appendChild(card)
+        console.log(card)        
+    });
+
+    filmes.forEach(filme => {
+        const card = criarCards(filme)
+        container2.appendChild(card)
+        console.log(card)        
+    });
+}
+
+mostrarCards();
